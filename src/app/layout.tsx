510c587de307cb3b380ef13f12f06291e877866b
allenk <ellenaponomarenko@gmail.com>
2025-05-02 21:58:40 +0300

    fix: selector :root is not pure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="root">
         <AntdProvider>
           <QCProvider>{children}</QCProvider>
         </AntdProvider>
